64e0bd7f4bd819b721664233e0f09f6c1e1652c6
thehannk <108367955+thehannk@users.noreply.github.com>
2022-07-10 02:04:30 +0300

    Girilen sayının karesini alma metotu

---
+++ SayiKaresiAlmaMetot/SayiKaresiAlmaMetot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayiKaresiAlmaMetot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir sayı giriniz :");
+            int girilenSayi = Convert.ToInt32(Console.ReadLine());
+
+            tekMiCiftMi(girilenSayi);   //ilk metotu yazıyorum, yani tek mi çift miyi. sonra yanındaki paranteze girilensayi yi parametre olarak verdim.
+
+            Console.ReadLine();
+        }
+
+        static void tekMiCiftMi(int sayi)
+        {
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Sayı tektir.");
+            }
+
+        }
+    }
+}
